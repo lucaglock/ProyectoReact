a266bba9b8fc87fa100cd165958f45a35e12f4cd
Luca Glock <lucacasm@gmail.com>
2023-10-11 20:52:13 -0300

    2da clase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const numero = 10;
+const styleRojo = {
+  backgroundColor:'red',
+};
+const styleAzul = {
+  backgroundColor:'blue',
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor: 'aquamarine', border: '2px solid black'}}>
+      <h1 className="titulo">Hola</h1>
+      <p>Mundo</p>
+      <p>{numero}</p>
+      <div>
+        <ul>
+          <li>papu</li>
+          <li>lince</li>
+        </ul>
+      </div>
     </div>
   );
 }
